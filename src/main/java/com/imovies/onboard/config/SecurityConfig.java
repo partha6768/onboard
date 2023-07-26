7bc24bda433fc6f9cb371df3e4764649a6e04318
Partha <50414553+partha6768@users.noreply.github.com>
2023-07-26 15:47:18 +0530

    Adding CORS Setting

---
+++ src/main/java/com/imovies/onboard/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.imovies.onboard.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+            }
+        };
+    }
+}
